bc15c3575079c21763a72bfdc522941e6693b306
Timothee LAPOV <artemlapov@me.com>
2024-04-16 16:59:20 +0200

    Je commence get/post

---
+++ transmission-data/block/navbar.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link p-3" href="index.php">INDEX</a>
+                <a class="nav-link p-3" href="get.php">GET</a>
+                <a class="nav-link p-3" href="post.php">POST</a>
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ transmission-data/get.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GET</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php
+    include("block/navbar.php");
+    ?>
+    <h1 class="text-center p-4">GET.PHP</h1>
+    <p>
+        <?php
+        var_dump($_GET);
+        ?>
+    </p>
+</body>
+
+</html>
