26177e0b15641eadacace851221b61bfba5d0702
Timothee LAPOV <artemlapov@me.com>
2024-04-17 16:43:52 +0200

    cafe/page-1

--- cafe/index.php
+++ cafe/index.php
@@ -12,7 +12,43 @@
     <?php include("../block/navbar.php"); ?>
     <h1 class="text-center text-bg-secondary">Moi c'est une machine a café</h1>
     <h2 class="text-center text-bg-success">Choisissez une boisson</h2>
-
+    <?php
+    $boissons = [
+        [
+            "img" => "img/espresso.webp",
+            "name" => "Espresso",
+            "price" => "1,5 €"
+        ],
+        [
+            "img" => "img/cappuccino.webp",
+            "name" => "Cappuccino",
+            "price" => "3,5 €"
+        ],
+        [
+            "img" => "img/chocolat.webp",
+            "name" => "Chocolat chaud",
+            "price" => "4,0 €"
+        ]
+    ];
+    ?>
+    <div class="container">
+        <div class="row text-center justify-content-center">
+            <?php
+            foreach ($boissons as $boisson) {
+                echo ("
+            <div class='card m-3' style='width: 18rem;'>
+                <img src='{$boisson['img']}' class='card-img-top' alt='{$boisson['name']}'>
+                <div class='card-body'>
+                    <h5 class='card-title'>{$boisson['name']}</h5>
+                    <p class='card-text'>{$boisson['price']}</p>
+                    <a href='options.php?boisson={$boisson['name']}' class='btn btn-primary'>Acheter</a>
+                </div>
+            </div>
+                ");
+            };
+            ?>
+        </div>
+    </div>
 </body>
 
 </html>
